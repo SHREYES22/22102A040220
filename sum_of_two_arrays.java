89c5fdd3e259f4300c445244afe01e20a03d87b0
SHREYES22 <saishreyesp@gmail.com>
2025-06-25 08:05:59 +0530

    Update and rename SUM OF TWO ARRAYS.java to sum_of_two_arrays.java

--- sum_of_two_arrays.java
+++ sum_of_two_arrays.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class sum_of_two_arrays{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
