c951f4c660eb0ad44b067738ecd7425a767faef1
SHREYES22 <saishreyesp@gmail.com>
2025-06-25 08:06:56 +0530

    Update and rename REVERSE OF AN ARRAY.java to reverse_of_an_array.java

--- reverse_of_an_array.java
+++ reverse_of_an_array.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class reverse_of_an_array{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
