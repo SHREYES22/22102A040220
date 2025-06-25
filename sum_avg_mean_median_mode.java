fb76c3c3accc6d074fcbce82251eb99f5c39a38a
SHREYES22 <saishreyesp@gmail.com>
2025-06-25 08:09:36 +0530

    Update and rename SUM,AVG,MEAN,MEDIAN AND MODE IN AN ARRAY.java to sum_avg_mean_median_mode.java

--- sum_avg_mean_median_mode.java
+++ sum_avg_mean_median_mode.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class sum_avg_mean_median_mode{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
