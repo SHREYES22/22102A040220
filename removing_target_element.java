8327d6d32ff841109f791a54d6c82b7e0a537417
SHREYES22 <saishreyesp@gmail.com>
2025-06-25 08:08:09 +0530

    Update and rename REMOVING TARGET ELEMENT  IN AN ARRAY.java to removing_target_element.java

--- removing_target_element.java
+++ removing_target_element.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class removing_target_element{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
